aaa68c5a537cee5fdbdbab2868644362844bfbb3
Alex Leeper <alixvox@gmail.com>
2021-10-07 23:12:52 -0500

    dsa

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /*
- * _strlen - returns the length of a string.
+ * strlen - returns the length of a string.
  *
  * @s: The string to be measured
  */
@@ -10,9 +10,10 @@
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	return i;
+	return (i);
 }
