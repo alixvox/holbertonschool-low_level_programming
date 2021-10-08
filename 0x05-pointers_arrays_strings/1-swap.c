788be88f63e319c16260795d25e94a4f4989fbfe
Alex Leeper <alixvox@gmail.com>
2021-10-07 23:04:18 -0500

    asdasd

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,14 @@
 
 /**
  * swap_int: swaps the values of two integers
+ * @s: The first  int
+ * @a: The second int
  */
 
 void swap_int(int *a, int *b)
 {
 	int s;
+
 	s = *a;
 	*a = *b;
 	*b = s;
