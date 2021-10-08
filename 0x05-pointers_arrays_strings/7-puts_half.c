66a603d3aca14033cb703c0da1e0d29dbe8fa2c2
Alex Leeper <alixvox@gmail.com>
2021-10-07 23:32:31 -0500

    asd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,16 +12,16 @@ void puts_half(char *str)
 	int i = 0;
 	int j;
 
-	while (str[i] != '\n')
+	while (str[i] != '\0')
 	{
 		i++;
 	}
-	
+
 	j = i / 2;
-	
-	while ( j<= i)
+
+	while (j<= i)
 	{
-		_putchar(str[j-1]);
+		_putchar(str[j - 1]);
 		j++;
 	}
 }
