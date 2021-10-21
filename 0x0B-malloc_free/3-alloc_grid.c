1921aec704d6d83ab7e7e2e90d401a8853a61c0e
alixvox <alixvox@gmail.com>
2021-10-20 20:55:20 -0700

    Dsa

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,10 +16,22 @@ int **alloc_grid(int width, int height)
 	int **s;
 	int i;
 
+	if (width < 1 || height < 1)
+		return (NULL);
 	s = malloc(height * sizeof(s));
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		s[i] = malloc(width * sizeof(int));
+		if (s[i] == NULL)
+		{
+			free(s[i]);
+			return (NULL);
+		}
 	}
 
 	return (s);
