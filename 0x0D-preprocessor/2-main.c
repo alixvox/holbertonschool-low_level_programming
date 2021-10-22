b5f6e9c8c9ea64a12e317bb3b6d8717bc5798a45
Alex Leeper <alixvox@gmail.com>
2021-10-21 21:51:17 -0500

    preprocessor project

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}
