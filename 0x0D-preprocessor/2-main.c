6252a5807ab14fffedb97cf12919353b7b00bcb4
Alex Leeper <alixvox@gmail.com>
2021-10-22 09:38:25 -0500

    das

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * main - prints the name of the file it was compiled from
@@ -10,7 +25,13 @@
 
 int main(void)
 {
-	printf(__FILE__);
-	printf("\n");
+	char *dest = __FILE__;
+
+	while (*dest != '\0')
+	{
+		_putchar(*dest);
+		dest++;
+	}
+	_putchar('\n');
 	return (0);
 }
