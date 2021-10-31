f1792edbab74088c4e21f01395c8a256796dbb80
Alex Leeper <alixvox@gmail.com>
2021-10-30 23:53:01 -0700

    wow

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -58,7 +58,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		return (EXIT_FAILURE);
+		exit(100);
 	}
 	return (a / b);
 }
@@ -77,7 +77,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		return (EXIT_FAILURE);
+		exit(100);
 	}
 	return (a % b);
 }
