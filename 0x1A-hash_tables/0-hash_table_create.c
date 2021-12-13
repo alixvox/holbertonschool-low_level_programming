f8b300823d346f5c2b7a9f733f79a9182cbe51bb
alixvox <alixvox@gmail.com>
2021-12-12 19:33:11 -0800

    bettying

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,8 +14,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 	{
-		free(table);
 		free(table->array);
+		free(table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
